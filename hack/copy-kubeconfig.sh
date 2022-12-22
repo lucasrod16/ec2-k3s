60ca8e9059fcb7cc3ebb9a00b1481f38c37e0ba4
Lucas Rodriguez <lucas.rodriguez@defenseunicorns.com>
2022-12-22 13:37:11 -0600

    Remove -i option from ssh and scp commands since it uses the one in use by default

--- hack/copy-kubeconfig.sh
+++ hack/copy-kubeconfig.sh
@@ -3,8 +3,8 @@
 public_ip="$(aws ec2 describe-instances --filters Name=instance-state-name,Values=running | jq -r '.Reservations[].Instances[].PublicIpAddress')"
 
 echo "Copying the kubeconfig from the ec2 instance..."
-ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -o IdentitiesOnly=yes ubuntu@"${public_ip}" 'kubectl config view --minify=true --raw=true > ./kubeconfig.yaml'
-scp -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -o IdentitiesOnly=yes ubuntu@"${public_ip}":./kubeconfig.yaml .
+ssh -o StrictHostKeyChecking=no -o IdentitiesOnly=yes ubuntu@"${public_ip}" 'kubectl config view --minify=true --raw=true > ./kubeconfig.yaml'
+scp -o StrictHostKeyChecking=no -o IdentitiesOnly=yes ubuntu@"${public_ip}":./kubeconfig.yaml .
 
 echo "Editing the cluster api-server IP address in the kubeconfig..."
 gsed -i "s|0\.0\.0\.0|${public_ip}|g" ./kubeconfig.yaml
