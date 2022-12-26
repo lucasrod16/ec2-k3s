f9b41726720657f748c6ace4b3beb5e75949b14d
Lucas Rodriguez <lucas.rodriguez@defenseunicorns.com>
2022-12-26 17:06:33 -0600

    Split creation of AWS resources into separate functions

--- internal/ec2/ec2.go
+++ internal/ec2/ec2.go
@@ -12,93 +12,128 @@ import (
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
-func Create() {
-	pulumi.Run(func(ctx *pulumi.Context) error {
-		// Create security group
-		sg, err := ec2.NewSecurityGroup(ctx, "lucas-dev-sg", &ec2.SecurityGroupArgs{
-			Description: pulumi.String("Allow all inbound traffic from the workstation IP address only"),
-			Ingress: ec2.SecurityGroupIngressArray{
-				&ec2.SecurityGroupIngressArgs{
-					Description: pulumi.String("All ports and protocols from workstation IP"),
-					FromPort:    pulumi.Int(0),
-					ToPort:      pulumi.Int(0),
-					Protocol:    pulumi.String("-1"),
-					CidrBlocks: pulumi.StringArray{
-						pulumi.String(localIP()),
-					},
-				},
-			},
-			Egress: ec2.SecurityGroupEgressArray{
-				&ec2.SecurityGroupEgressArgs{
-					FromPort: pulumi.Int(0),
-					ToPort:   pulumi.Int(0),
-					Protocol: pulumi.String("-1"),
-					CidrBlocks: pulumi.StringArray{
-						pulumi.String("0.0.0.0/0"),
-					},
-				},
-			},
-			Tags: pulumi.StringMap{
-				"Name": pulumi.String("allow all ports and protocols from workstation IP"),
-			},
-		})
-		if err != nil {
-			return err
-		}
+type infrastructure struct {
+	ami           *ec2.LookupAmiResult
+	Keypair       *ec2.KeyPair
+	SecurityGroup *ec2.SecurityGroup
+	Server        *ec2.Instance
+}
 
-		// Create SSH keypair
-		keypair, err := ec2.NewKeyPair(ctx, "lucas-dev-ssh", &ec2.KeyPairArgs{
-			KeyName:   pulumi.String("ec2-k3d-keypair"),
-			PublicKey: pulumi.String(getPublicSSHKey()),
-		})
-		if err != nil {
-			return err
-		}
-		fmt.Println(keypair)
-
-		// Get Ubuntu 22.04 AMI ID
-		ubuntu, err := ec2.LookupAmi(ctx, &ec2.LookupAmiArgs{
-			MostRecent: pulumi.BoolRef(true),
-			Filters: []ec2.GetAmiFilter{
-				ec2.GetAmiFilter{
-					Name: "name",
-					Values: []string{
-						"ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*",
-					},
-				},
-				ec2.GetAmiFilter{
-					Name: "virtualization-type",
-					Values: []string{
-						"hvm",
-					},
+// createSecurityGroup creates a security group in AWS
+func createSecurityGroup(ctx *pulumi.Context) (*infrastructure, error) {
+	securityGroup, err := ec2.NewSecurityGroup(ctx, "security-group", &ec2.SecurityGroupArgs{
+		Description: pulumi.String("Allow all inbound traffic from the workstation IP address only"),
+		Ingress: ec2.SecurityGroupIngressArray{
+			&ec2.SecurityGroupIngressArgs{
+				Description: pulumi.String("All ports and protocols from workstation IP"),
+				FromPort:    pulumi.Int(0),
+				ToPort:      pulumi.Int(0),
+				Protocol:    pulumi.String("-1"),
+				CidrBlocks: pulumi.StringArray{
+					pulumi.String(localIP()),
 				},
 			},
-			Owners: []string{
-				"099720109477",
+		},
+		Egress: ec2.SecurityGroupEgressArray{
+			&ec2.SecurityGroupEgressArgs{
+				FromPort: pulumi.Int(0),
+				ToPort:   pulumi.Int(0),
+				Protocol: pulumi.String("-1"),
+				CidrBlocks: pulumi.StringArray{
+					pulumi.String("0.0.0.0/0"),
+				},
 			},
-		}, nil)
-		if err != nil {
-			return err
-		}
-
-		// Create ec2 instance
-		_, err = ec2.NewInstance(ctx, "lucas-dev-ec2", &ec2.InstanceArgs{
-			Ami:                 pulumi.String(ubuntu.Id),
-			InstanceType:        pulumi.String("t3.2xlarge"),
-			KeyName:             pulumi.String("ec2-k3d-keypair"),
-			VpcSecurityGroupIds: pulumi.StringArray{sg.ID()},
-			Tags: pulumi.StringMap{
-				"Name":         pulumi.String("lucas-dev"),
-				"Cluster-type": pulumi.String("k3d"),
-				"Workload":     pulumi.String("bigbang"),
-			},
-			UserData: pulumi.String(getUserDataScript()),
-		})
-		if err != nil {
-			return err
-		}
-		return nil
+		},
+		Tags: pulumi.StringMap{
+			"Name": pulumi.String("allow all ports and protocols from workstation IP"),
+		},
 	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &infrastructure{
+		SecurityGroup: securityGroup,
+	}, nil
+}
+
+// CreateSSHKeyPair creates an SSH keypair in AWS
+func CreateSSHKeyPair(ctx *pulumi.Context) (*infrastructure, error) {
+	keypair, err := ec2.NewKeyPair(ctx, "ssh-keypair", &ec2.KeyPairArgs{
+		KeyName:   pulumi.String("ec2-k3d-keypair"),
+		PublicKey: pulumi.String(getPublicSSHKey()),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &infrastructure{
+		Keypair: keypair,
+	}, nil
+}
+
+// CreateInstance creates an ec2 instance in AWS
+func CreateInstance(ctx *pulumi.Context) (*infrastructure, error) {
+	computeInfra, err := getUbuntuAMI(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	securityInfra, err := createSecurityGroup(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	server, err := ec2.NewInstance(ctx, "ec2-instance", &ec2.InstanceArgs{
+		Ami:                 pulumi.String(computeInfra.ami.ImageId),
+		InstanceType:        pulumi.String("t3.2xlarge"),
+		KeyName:             pulumi.String("ec2-k3d-keypair"),
+		VpcSecurityGroupIds: pulumi.StringArray{securityInfra.SecurityGroup.ID()},
+		Tags: pulumi.StringMap{
+			"Name":         pulumi.String("lucas-dev"),
+			"Cluster-type": pulumi.String("k3d"),
+			"Workload":     pulumi.String("bigbang"),
+		},
+		UserData: pulumi.String(getUserDataScript()),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &infrastructure{
+		Server: server,
+	}, nil
+}
+
+// getUbuntuAMI returns the latest Ubuntu 22.04 AMI ID
+func getUbuntuAMI(ctx *pulumi.Context) (*infrastructure, error) {
+	ami, err := ec2.LookupAmi(ctx, &ec2.LookupAmiArgs{
+		MostRecent: pulumi.BoolRef(true),
+		Filters: []ec2.GetAmiFilter{
+			ec2.GetAmiFilter{
+				Name: "name",
+				Values: []string{
+					"ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*",
+				},
+			},
+			ec2.GetAmiFilter{
+				Name: "virtualization-type",
+				Values: []string{
+					"hvm",
+				},
+			},
+		},
+		Owners: []string{
+			"099720109477",
+		},
+	}, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return &infrastructure{
+		ami: ami,
+	}, nil
 }
 
 // localIP returns the IP address of the machine that executed the program
@@ -107,15 +142,20 @@ func localIP() []byte {
 	if err != nil {
 		panic(err)
 	}
+
 	defer resp.Body.Close()
+
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		panic(err)
 	}
+
 	trimmedBody := bytes.Trim(body, "\n")
 	suffix := "/32"
 	cidr := append([]byte(trimmedBody), suffix...)
+
 	fmt.Printf("\nWorkstation IP address: %s", cidr)
+
 	return cidr
 }
 
@@ -125,6 +165,7 @@ func getPublicSSHKey() []byte {
 	if err != nil {
 		panic(err)
 	}
+
 	publicSSHKey := userHomeDir + "/.ssh/id_rsa.pub"
 	keyData, err := os.ReadFile(publicSSHKey)
 	if err != nil {
--- main.go
+++ main.go
@@ -1,8 +1,29 @@
 package main
 
-import "main/internal/ec2"
+import (
+	"main/internal/ec2"
+
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
 
 func main() {
-	// Create security group, ec2 instance, and ssh key pair
-	ec2.Create()
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		// Create SSH keypair
+		_, err := ec2.CreateSSHKeyPair(ctx)
+		if err != nil {
+			return err
+		}
+
+		// Create ec2 instance and security group
+		infra, err := ec2.CreateInstance(ctx)
+		if err != nil {
+			return err
+		}
+
+		// Print infrastructure details to stdout
+		ctx.Export("EC2 Instance", infra.Server.ID())
+		ctx.Export("publicIp", infra.Server.PublicIp)
+		ctx.Export("publicHostName", infra.Server.PublicDns)
+		return nil
+	})
 }
