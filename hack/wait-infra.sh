e0ac91b146a65093bf430fc47af4a5ce72822939
Lucas Rodriguez <lucas.rodriguez@defenseunicorns.com>
2022-12-28 22:02:00 -0600

    Remove aws wait command from wait-infra.sh

--- hack/wait-infra.sh
+++ hack/wait-infra.sh
@@ -2,7 +2,6 @@
 
 INSTANCE_ID="$(aws ec2 describe-instances --filters Name=instance-state-name,Values=running | jq -r '.Reservations[].Instances[].InstanceId')"
 
-aws ec2 wait instance-running --output json --no-cli-pager --instance-ids "${INSTANCE_ID}" &> /dev/null
 until [ "$(aws ec2 describe-instance-status --instance-ids "${INSTANCE_ID}" | jq -r '.InstanceStatuses[].InstanceStatus.Details[].Status')" == "passed" ]
 do
     echo "Waiting for the ec2 instance to be ready...🤖"
